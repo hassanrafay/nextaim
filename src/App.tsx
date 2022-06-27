4c891c5b3b9476de0d53ba6c9ce8c1e8f7628866
Rafay Hassan <rafay.hassan@outlook.com>
2022-06-27 13:32:47 +0300

    installed mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,8 @@
 import React from 'react';
+import { Button } from '@mui/material';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Button>Hello!</Button>;
 }
 
 export default App;
