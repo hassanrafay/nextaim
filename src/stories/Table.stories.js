704874dadae7939f026c635690cc1a69ce6de8d0
Rafay Hassan <rafay.hassan@outlook.com>
2022-06-28 12:58:21 +0300

    storybook configuration

---
+++ src/stories/Table.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ClientTable from '../components/ClientTable';
+
+export default {
+  title: 'Client Table',
+  component: ClientTable
+};
+
+export const Initial = <ClientTable />;
